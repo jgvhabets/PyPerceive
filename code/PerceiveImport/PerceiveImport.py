60acce6bc7603b2abd9a65cd78641f16c075a971
jenniferbehnke <113339299+jenniferbehnke@users.noreply.github.com>
2022-11-05 19:42:03 +0100

    defined select_matfiles method in PerceiveImport

--- code/PerceiveImport/PerceiveImport.py
+++ code/PerceiveImport/PerceiveImport.py
@@ -4,6 +4,8 @@
 import os 
 from dataclasses import dataclass
 
+import json
+
 # import PerceiveImport.filefunctions as filefuncs -> use this line to import other .py files
 
 @dataclass(init=True, repr=True) 
@@ -33,20 +35,28 @@ class PerceiveData:
         
     def __str__(self,):
         return f'From the Perceived data from subject {self.sub} all BrainSense {self.data_type} .mat files from the {self.timing} session are being selected.'
-    
 
-    # find all matfiles within the correct timing folder
-    def find_matfiles(self,):
-        for root, dirs, files in os.walk(self.data_path):
-            for file in files:
-                if self.timing in root and file.endswith(".mat"):
-                    print(file)
-    
+             
+    # select matfiles only for of correct datatype    
+    def select_matfiles(self,):
+        matfile_list = [] # this list will contain all matfile names
+        paths_list = [] # this list will contain all paths to the selected matfiles
+
+        with open('matpart.json', 'r') as f: # read matpart.json file with dictionary of datatypes
+            matpart = json.loads(f.read())
+
+        for root, dirs, files in os.walk(self.data_path): # walking through every root, directory and file of the given path
+            for file in files: # looping through every file 
+                if self.timing in root and file.endswith(".mat") and matpart[self.data_type] in file: # matpart is defined earlier
+                    print(file) # printing makes sure, that every selected file is saved after a loop
+                    matfile_list.append(file) # adding every file to the list of matfile names
+            
+                    single_path = os.path.join(root, file) # keep root and file joined together so the path won´t get lost
+                    paths_list.append(single_path) # add each path to the list selection_paths
         
-    # select matfiles only for of correct datatype
-    # therefore load matpart.json file with dictionary
-    # def select_datatypefiles():
-        
+        return f'Filenames of the selected .mat files: \n\t{matfile_list} \n\n\tList of paths to the selected .mat files: \n\t{paths_list}'
+         
+
     
     # load the final selection of files into the correct datatype structure
     # e.g. load files from .selected_datatypefiles() into Survey_class.py
