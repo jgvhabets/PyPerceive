dc10f5c2b483a2fdc6d48f731b263fc4517b8b15
jenniferbehnke <113339299+jenniferbehnke@users.noreply.github.com>
2022-11-06 00:13:37 +0100

    first steps datatype subclasses

--- code/PerceiveImport/Streaming_class.py
+++ code/PerceiveImport/Streaming_class.py
@@ -1,1 +1,57 @@
 """ Create a Streaming Class """
+
+import os
+from dataclasses import dataclass
+
+import mne_bids
+import mne
+
+@dataclass (init=True, repr=True)
+class Streaming:
+    """
+    BrainSense Streaming Class 
+    
+    parameters: ???? are defined by loading data in ???
+        - ch_names: channel names
+        - n_chan: number of channels
+        - n_time_samps: number of samples
+        - time_secs: timepoints in seconds
+        - sampling_freq: sampling frequency
+        - time_duration: duration of the recording
+    
+    Returns:
+        - 
+    
+    """
+    
+    # initialized fields
+    
+    
+    
+    
+    def __post_init__(self,):
+        # run load_matfile method??
+        
+        # ch_names = raw.ch_names
+        # n_chan = len(ch_names)
+        # n_time_samps = raw.n_times #nsamples
+        # time_secs = raw.times #timepoints set to zero
+        # ch_trials = raw._data
+        # sampling_freq = raw.info['sfreq']
+        # time_duration = (n_time_samps/sampling_freq).astype(float)
+    
+    def __str__(self,):
+        #return f'channel names are {self.ch_names}'
+    
+    # return for every loaded file:
+    # print(
+    #   f'The data object has:\n\t{n_time_samps} time samples,'
+    #   f'\n\tand a sample frequency of {sampling_freq} Hz' 
+    #   f'\n\twith a recording duration of {time_duration} seconds.' 
+    #   f'\n\t{n_chan} channels were labeled as \n{ch_names}.'
+    #   )
+    
+
+    
+    
+    
--- code/PerceiveImport/Survey_class.py
+++ code/PerceiveImport/Survey_class.py
@@ -1,1 +1,43 @@
 """ Create Survey class """
+
+import os
+import sys
+import importlib
+import json
+from dataclasses import dataclass, field, fields
+from itertools import compress
+import csv
+import pandas as pd
+import numpy as np
+
+import sklearn as sk
+import scipy
+import matplotlib.pyplot as plt
+from scipy import signal
+
+#mne
+import mne_bids
+import mne
+
+@dataclass (init=True, repr=True)
+class Survey:
+    """
+    BrainSense Survey Class 
+    
+    parameters:
+        - 
+    
+    Returns:
+        - 
+    
+    """
+    
+    # initialized fields
+    ch_names
+    
+    
+    def __post_init__(self,):
+        print(self.ch_names)
+    
+    def __str__(self,):
+        return f'channel names are {self.ch_names}'
