e228c6764b9ba0ec97832009e6e15feeed0c62dd
jenniferbehnke <113339299+jenniferbehnke@users.noreply.github.com>
2022-11-06 00:13:01 +0100

    edited select matfile methods

--- code/PerceiveImport/PerceiveImport.py
+++ code/PerceiveImport/PerceiveImport.py
@@ -6,7 +6,9 @@ from dataclasses import dataclass
 
 import json
 
-# import PerceiveImport.filefunctions as filefuncs -> use this line to import other .py files
+# import PerceiveImport.select_matfiles as matfiles   # use this line to import other .py files
+# import importlib
+# importlib.reload(matfiles) 
 
 @dataclass(init=True, repr=True) 
 class PerceiveData:
@@ -19,7 +21,8 @@ class PerceiveData:
         - timing: timing of session, e.g. "3MFU", "12MFU", "Postop"
         - data_type: choose between "Survey", "Streaming", "Timeline"
         
-    Returns    
+    Returns:
+        - 
     """
     
     # these fields will be initialized 
@@ -28,17 +31,68 @@ class PerceiveData:
     timing: str
     data_type: str
         
-    # note the comma after self!
+    # note that every defined method contains (self,) don´t forget the comma after self!
     def __post_init__(self,):  # post__init__ function runs after class initialisation
         print(self.sub)
         self.files=os.listdir(self.data_path) # self.files can only run after initialisation because it needs self.data_path
         
+        # selected_matfiles = matfiles.select_matfiles()
+        
     def __str__(self,):
         return f'From the Perceived data from subject {self.sub} all BrainSense {self.data_type} .mat files from the {self.timing} session are being selected.'
 
-             
-    # select matfiles only for of correct datatype    
-    def select_matfiles(self,):
+    # def select_matfiles(self,):
+    #     matfinal = matfiles.select_matfinal(self,)
+    #     print(matfinal)
+ 
+    # select all .mat files in respect of the correct datatype (Survey/Streaming/Timeline) 
+    def select_matdatatype(self,):
+        matfile_list = [] # this list will contain all matfile names
+        paths_list = [] # this list will contain all paths to the selected matfiles
+
+        with open('matpart.json', 'r') as f: # read matpart.json file with dictionary of datatypes
+            matpart = json.loads(f.read())
+
+        for root, dirs, files in os.walk(self.data_path): # walking through every root, directory and file of the given path
+            for file in files: # looping through every file 
+                if file.endswith(".mat") and matpart[self.data_type] in file: # matpart is defined earlier
+                    print(file) # printing makes sure, that every selected file is saved after a loop
+                    matfile_list.append(file) # adding every file to the list of matfile names
+            
+                    single_path = os.path.join(root, file) # keep root and file joined together so the path won´t get lost
+                    paths_list.append(single_path) # add each path to the list selection_paths
+        
+        print('\n\tFilenames of the selected .mat files:') 
+        print(matfile_list)
+        
+        print('\n\tList of paths to the selected .mat files:') 
+        print(paths_list)
+
+
+    # select all .mat files in respect of the correct timing (3MFU/12MFU/Postop)
+    def select_mattiming(self,):
+        matfile_list = [] # this list will contain all matfile names
+        paths_list = [] # this list will contain all paths to the selected matfiles
+
+        for root, dirs, files in os.walk(self.data_path): # walking through every root, directory and file of the given path
+            for file in files: # looping through every file 
+                if self.timing in root and file.endswith(".mat"):
+                    print(file) # printing makes sure, that every selected file is saved after a loop
+                    matfile_list.append(file) # adding every file to the list of matfile names
+            
+                    single_path = os.path.join(root, file) # keep root and file joined together so the path won´t get lost
+                    paths_list.append(single_path) # add each path to the list selection_paths
+        
+        print('\n\tFilenames of the selected .mat files:') 
+        print(matfile_list)
+        
+        print('\n\tList of paths to the selected .mat files:') 
+        print(paths_list)
+    
+    
+    # selecting final selection of .mat files
+    # in respect of timing and datatype
+    def select_matfinal(self,):
         matfile_list = [] # this list will contain all matfile names
         paths_list = [] # this list will contain all paths to the selected matfiles
 
@@ -54,13 +108,21 @@ class PerceiveData:
                     single_path = os.path.join(root, file) # keep root and file joined together so the path won´t get lost
                     paths_list.append(single_path) # add each path to the list selection_paths
         
-        return f'Filenames of the selected .mat files: \n\t{matfile_list} \n\n\tList of paths to the selected .mat files: \n\t{paths_list}'
-         
-
+        print('\n\tFilenames of the selected .mat files:') 
+        print(matfile_list)
+        
+        print('\n\tList of paths to the selected .mat files:') 
+        print(paths_list)
+    
     
     # load the final selection of files into the correct datatype structure
-    # e.g. load files from .selected_datatypefiles() into Survey_class.py
-    # def load_finalselection():
+    # e.g. load files from .select_matfiles() into Survey_class.py
+    def load_selection(self,):
+        for file in paths_list: # paths_list is only defined after running select_matfinal() method ?!?
+            raw = mne.io.read_raw_fieldtrip(paths_list[file],info={},data_name='data',)
+        
+    
+    
     
     
         # xxx = filefuncs.FUNCTIon()
