868ea3c2bf84f6d7695e7bfda6d195b020d6023d
JKBehnke <jennifer-kim.behnke@t-online.de>
2022-11-25 10:22:52 +0100

    defined function get onedrive folder()

--- code/PerceiveImport/methods/find_folders.py
+++ code/PerceiveImport/methods/find_folders.py
@@ -26,7 +26,7 @@ def get_onedrive_path(
     """
 
     folder_options = [
-        'onedrive', 'figures', 'perceivedata', 'results'
+        'onedrive', 'perceivedata', 'results'
         ]
 
     if folder.lower() not in folder_options:
